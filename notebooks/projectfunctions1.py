67e993e05918425023597866af4fd4bfc87b2e3b
austinwayling <austinwayling@gmail.com>
2021-10-31 11:31:11 -0700

    updated functions and testing things

--- notebooks/projectfunctions1.py
+++ notebooks/projectfunctions1.py
@@ -20,7 +20,8 @@ def batting_load_and_process(file_path):
                     BA = lambda x: x['H'] / x['AB'],
                     OBP = lambda x: (x['H'] + x['BB'] + x['HBP']) / x['PA'],
                     SLG = lambda x: ((x['H'] - x['2B'] - x['3B'] - x['HR']) + 2 * x['2B'] + 3 * x['3B'] + 4 * x['HR']) / x['AB'],
-                    OPS = lambda x: x['OBP'] + x["SLG"]
+                    OPS = lambda x: x['OBP'] + x["SLG"],
+                    decade = lambda x: (x.index//10)*10
                 )
         )
     return bat_clean_year
@@ -35,7 +36,9 @@ def pitching_load_and_process(file_path):
             pitching_clean.groupby('yearID').sum()
             .drop(["stint", "BK", "GF", "SH", "SF", "GIDP", "BAOpp", "ERA", "W", "L", "G", "GS", "BFP"], axis = 1)
             .assign(
-                ERA = lambda x: (9 * x['ER']) / (x['IPouts'] / 3)
+                ERA = lambda x: (9 * x['ER']) / (x['IPouts'] / 3),
+                KPG = lambda x: 9 * x['SO']/(x['IPouts']/3),
+                BBPG = lambda x: 9 * x['BB']/(x['IPouts']/3)
             )
         )
     return pitch_clean_year
@@ -43,5 +46,16 @@ def pitching_load_and_process(file_path):
 
 def merge_batting_pitching(bat, pitch):
     diff_cols = pitch.columns.difference(bat.columns)
-    data_clean = pd.merge(bat, pitch[diff_cols], on = ['yearID'])
+    data_clean = (
+        pd.merge(bat, pitch[diff_cols], on = ['yearID'])
+        .assign(
+            BABIP = lambda x: (x['H'] - x['HR']) / (x['AB']-x['SO']-x['HR']) 
+        )
+        .reset_index()
+    )
     return data_clean
+
+
+def normal_data(df):
+    dfn=((df-df.min())/(df.max()-df.min()))*20 
+    return dfn
